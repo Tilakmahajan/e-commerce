8e0ad593209bed76dfc13585cef26ad120c02525
Tilak Mahajan <tilakmahajan1610@gmail.com>
2025-09-10 23:36:08 +0530

    updated the layout of app

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Footer from "@/app/components/Footer";
 import Script from "next/script";
 import { CartProvider } from "@/app/components/CartContext";
 import { AuthProvider } from "./components/AuthContext";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "max-wholesaler",
@@ -14,20 +15,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-          <AuthProvider>
-    
-        <CartProvider>
-          <Navbar />
-         
-          <main className="min-h-screen bg-gray-50">{children}</main>
-          <Script
-          src="https://checkout.razorpay.com/v1/checkout.js"
-          strategy="afterInteractive"
-        />
-        
-          <Footer />
-        </CartProvider>
-</AuthProvider>
+        <AuthProvider>
+          <CartProvider>
+            <Suspense fallback={null}>
+              <Navbar />
+            </Suspense>
+            <Suspense fallback={null}>
+              <main className="min-h-screen bg-gray-50">{children}</main>
+            </Suspense>
+            <Script
+              src="https://checkout.razorpay.com/v1/checkout.js"
+              strategy="afterInteractive"
+            />
+            <Footer />
+          </CartProvider>
+        </AuthProvider>
       </body>
     </html>
   );
