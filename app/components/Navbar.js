01bc051e0d5ba7f855bfa49eb225fddd3b20477b
Siddheshwar Jadhav <siddheshwarjadhav276@gmail.com>
2025-12-28 17:50:38 +0530

    update logo

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -6,6 +6,7 @@ import { useAuth } from "./AuthContext";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import { useEffect, useState, useRef } from "react";
 import { ShoppingCart, User, Mail } from "lucide-react";
+import Image from "next/image";
 
 export default function Navbar() {
   const { cart } = useCart();
@@ -53,12 +54,12 @@ export default function Navbar() {
     <nav className="sticky top-0 z-50 bg-white shadow-md border-b">
       <div className="container mx-auto px-4 md:px-6 py-3 flex flex-wrap items-center justify-between gap-3">
         {/* Logo */}
-        <Link
+       <Link
           href="/"
           className="text-2xl md:text-3xl font-extrabold text-gray-900 hover:text-blue-600 transition whitespace-nowrap"
         >
-          <span className="md:inline hidden">🛍️ MyStore</span>
-          <span className="md:hidden inline">🛍️</span>
+          <span className="md:inline hidden flex-row"><img className="h-14 border rounded-4xl" src="/logo.jpg" alt="" /></span>
+          <span className="md:hidden inline"><img className=" h-10 border rounded-4xl" src="/logo.jpg" alt="" /></span>
         </Link>
 
         {/* Search bar */}
